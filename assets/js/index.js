277b4de21a4af38769226194da528e30b65ba2d7
LuDon89 <daudonlucas@gmail.com>
2023-01-18 13:03:40 +0100

    base css desktop

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,7 +11,6 @@
 /* ==================== AJOUT DE LA CARTE ==================== */
 
 let mapCont = document.querySelector("#map");
-console.log(mapCont);
 
 let map = L.map('map').setView([51.505, -0.09], 13);
 
@@ -19,3 +18,27 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+
+
+/* ==================== TUTO REQUETE AJAX ==================== */
+
+const url = "https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=ensemble-des-lieux-de-restauration-des-crous&q=&rows=20&facet=type&facet=zone";
+
+fetch(url)
+  .then((resp) => resp.json())
+  .then((resp) => {
+    // Traitement JS
+    const restos = resp.records;
+    console.log(restos[0]);
+    // On fait une boucle pour lire les infos du tableau
+    for (let item of restos) {
+      console.log(item.fields.title);
+      console.log(item.fields.geolocalisation);
+    }
+
+  })
+  .catch((err) => console.log("Erreur de type :" + err));
+
+
+
