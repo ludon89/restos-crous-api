5dcefef84cdf61fc47622fe29559496a75b520db
LuDon89 <daudonlucas@gmail.com>
2023-01-18 16:31:41 +0100

    ajout des marqueurs

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,20 +8,24 @@
 
 
 
+
 /* ==================== AJOUT DE LA CARTE ==================== */
 
 let mapCont = document.querySelector("#map");
 
-let map = L.map('map').setView([51.505, -0.09], 13);
+let map = L.map('map').setView([50.633, 3.064156], 12);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+// let marker = L.marker([50.631389, 3.058319]).addTo(map);
 
 
-/* ==================== TUTO REQUETE AJAX ==================== */
+
+
+/* ==================== REQUETE AJAX ==================== */
 
 const url = "https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=ensemble-des-lieux-de-restauration-des-crous&q=&rows=20&facet=type&facet=zone";
 
@@ -30,11 +34,17 @@ fetch(url)
   .then((resp) => {
     // Traitement JS
     const restos = resp.records;
-    console.log(restos[0]);
+    console.log(restos);
+
     // On fait une boucle pour lire les infos du tableau
+    // for (let item of restos) {
+    //   console.log(item.geometry.coordinates);
+    // }
+
+    // Boucle pour lire toutes les coordonnées (et les noms des lieux) et mettre les marqueurs
     for (let item of restos) {
-      console.log(item.fields.title);
-      console.log(item.fields.geolocalisation);
+      let markerResto = L.marker([item.geometry.coordinates[1], item.geometry.coordinates[0]]).addTo(map)
+        .bindPopup(item.fields.title);
     }
 
   })
